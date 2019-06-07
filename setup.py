828f9b18908bc2a32a1634eee2de337ebc68b497
Greg Badros <badros@gmail.com>
2019-06-07 09:12:13 -0700

    ignore test-env, bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyketra',
-    version='0.0.2',
+    version='0.0.3',
     license='MIT',
     description='Python library for Ketra Controller',
     author='Greg J. Badros',
